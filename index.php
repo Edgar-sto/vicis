ef3c783641e57319e995a275acd7505814fe7210
Edgar Dueñas <edmadu12@gmail.com>
2020-01-28 20:17:37 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Facturación</title>
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/estilos.css">
+        <!-- Javascript -->
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script> 
+        <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <!--script src="js/funciones.js" charset="utf-8"></script-->
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2">
+                    <!--General botones-->
+                    <?php
+                        $servidor = array(5, 6, 8, 9, 14, 16, 35, 38, 42, 43, 45, 22, 27, 28, 29, 36, 39, 41, 44, 46, 201);
+                        $tamanio_array_servidor = count($servidor);
+                        for ($i=0; $i < $tamanio_array_servidor; $i++) {
+                            echo "<input class=\"btn btn-primary btn-m btn-block\" type=\"button\" id=\"server_$servidor[$i]\" value=\"Server $servidor[$i]\"/>";
+                        }
+                    ?>
+                </div>
+                <div class="col-md-1">
+                    <script type="text/javascript">
+                        $(document).ready(function(){
+                            <?php
+                                for ($j=0; $j < $tamanio_array_servidor; $j++) {
+                                    echo "$('#server_$servidor[$j]').click(function(){";
+                                    for ($k=0; $k < $tamanio_array_servidor; $k++) {
+                                        if ($servidor[$k] == $servidor[$j]) {
+                                            echo "$('#ocultar_ser_$servidor[$j]').each(function() {
+                                            displaying = $(this).css('display');
+                                            if(displaying == 'block') {
+                                                $(this).fadeOut('slow',function() {
+                                                    $(this).css('display','none');
+                                                });
+                                            } else {
+                                                $(this).fadeIn('slow',function() {
+                                                    $(this).css('display','block');
+                                                });
+                                            }
+                                            });
+                                        });";
+                                        } else {
+                                            echo "$('#ocultar_ser_$servidor[$k]').hide();";
+                                        }    
+                                    }
+                                }
+                            ?>
+                        });
+                    </script>
+                    <!--Server5-->
+                    <div id="ocultar_ser_5" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="820px" data="http://10.9.2.5/vicidial/welcome.php"></object>
+                    </div> 
+                    <!--Server6-->
+                    <div id="ocultar_ser_6" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.6/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server8-->
+                    <div id="ocultar_ser_8" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.8/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server9-->
+                    <div id="ocultar_ser_9" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.9/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server14-->
+                    <div id="ocultar_ser_14" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.14/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server16-->
+                    <div id="ocultar_ser_16" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.16/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server35-->
+                    <div id="ocultar_ser_35" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.35/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server38-->
+                    <div id="ocultar_ser_38" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.38/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server42-->
+                    <div id="ocultar_ser_42" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.42/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server43-->
+                    <div id="ocultar_ser_43" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.43/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server45-->
+                    <div id="ocultar_ser_45" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.45/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Sucursales Externas-->
+                    <!--Server22-->
+                    <div id="ocultar_ser_22" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.22/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server27-->
+                    <div id="ocultar_ser_27" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.27/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server28-->
+                    <div id="ocultar_ser_28" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.28/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server29-->
+                    <div id="ocultar_ser_29" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.29/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server36-->
+                    <div id="ocultar_ser_36" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.36/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server39-->
+                    <div id="ocultar_ser_39" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.39/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server41-->
+                    <div id="ocultar_ser_41" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.41/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server44-->
+                    <div id="ocultar_ser_44" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.44/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server46-->
+                    <div id="ocultar_ser_46" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.46/vicidial/welcome.php"></object>  
+                    </div>
+                    <!--Server201-->
+                    <div id="ocultar_ser_201" style="background:#000000;display:none;width:1000px;height:100px;">
+                        <object type="text/html" width="100%" height="460" data="http://10.9.2.201/vicidial/welcome.php"></object>  
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </body>
+</html>
---
+++ js/funciones.js
@@ -0,0 +1,360 @@
+$(document).ready(function(){
+    $("#server_5").click(function(){
+        $("#ocultar_ser_5").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_6").click(function(){
+         //$("#5").hide();
+          $("#ocultar_ser_5").hide();
+        $("#ocultar_ser_6").each(function() {
+
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_8").click(function(){
+        $("#ocultar_ser_8").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_9").click(function(){
+        $("#ocultar_ser_9").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_14").click(function(){
+        $("#ocultar_ser_14").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_16").click(function(){
+        $("#ocultar_ser_16").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_35").click(function(){
+        $("#ocultar_ser_35").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_38").click(function(){
+        $("#ocultar_ser_38").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_42").click(function(){
+        $("#ocultar_ser_42").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_43").click(function(){
+        $("#ocultar_ser_43").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_45").click(function(){
+        $("#ocultar_ser_45").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+/*Sucursales Externas*/
+$(document).ready(function(){
+    $("#server_22").click(function(){
+        $("#ocultar_ser_22").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_27").click(function(){
+        $("#ocultar_ser_27").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_28").click(function(){
+        $("#ocultar_ser_28").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_29").click(function(){
+        $("#ocultar_ser_29").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_36").click(function(){
+        $("#ocultar_ser_36").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_39").click(function(){
+        $("#ocultar_ser_39").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_41").click(function(){
+        $("#ocultar_ser_41").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_44").click(function(){
+        $("#ocultar_ser_44").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_46").click(function(){
+        $("#ocultar_ser_46").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
+
+$(document).ready(function(){
+    $("#server_201").click(function(){
+        $("#ocultar_ser_201").each(function() {
+            displaying = $(this).css("display");
+            if(displaying == "block") {
+                $(this).fadeOut('slow',function() {
+                    $(this).css("display","none");
+                });
+            } else {
+                $(this).fadeIn('slow',function() {
+                    $(this).css("display","block");
+                });
+            }
+        });
+    });
+});
