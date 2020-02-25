3561f49b2bc4035a63020bfec14fec378991274d
Edgar Dueñas <edmadu12@gmail.com>
2020-02-24 20:16:40 -0600

    .

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                 <a class="nav-link" href="http://127.0.0.1/facturacion/index.php" target="_blank">Facturación</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="http://127.0.0.1/vicis/factura/datos_factura.php" target="_blank">Datos Facturación</a>
+                <a class="nav-link" href="http://127.0.0.1/vicis/factura/datos_factura.php" target="_blank">Teléfonia</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="http://127.0.0.1/vicis/panelvicis/panel_vicis.php" target="_blank">Vicis Dial</a>
