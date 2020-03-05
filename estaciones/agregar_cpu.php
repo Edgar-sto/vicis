9705781cb29dc84bf42915227cd4d5f13f773c6b
Edgar Dueñas <edmadu12@gmail.com>
2020-03-04 20:55:48 -0600

    agregar cpu

--- estaciones/agregar_cpu.php
+++ estaciones/agregar_cpu.php
@@ -18,25 +18,16 @@
 
 	$conectar 		= 	mysqli_connect($servidor, $usuario, $pass, $basededatos);
 
-	$insertar ="SELECT MAX(uniqueid) AS id FROM cpu";
-	$query = mysqli_query($conectar, $insertar);
-	if ($row = mysql_fetch_row($query)) {
-		$id = trim($row[0]);
-		echo "$id";
-	}
-
-
-/*
-	$insertar = "INSERT INTO cpu (uniqueid, marca_cpu, serie_cpu, modelo_cpu, mac_address, ubicacion, comentario) VALUES ( '$marcaCpu', '$numSerieCpu', '$modeloCpu', '$mac', '$ubicacion', '$comentary')";
+	$insertar = "INSERT INTO cpu (marca_cpu, serie_cpu, modelo_cpu, mac_address, ubicacion, comentario) VALUES ( '$marcaCpu', '$numSerieCpu', '$modeloCpu', '$mac', '$ubicacion', '$comentary')";
 	$query = mysqli_query($conectar, $insertar);
 	if ($query) {
 		echo "<script>
 				alert('Equipo guardado');
-				
+				window.close();
 			  </script>";
 	} else {
 		echo "<script>
 				alert('No se guardo');
 			  </script>";
-	}*/
+	}
  ?>
