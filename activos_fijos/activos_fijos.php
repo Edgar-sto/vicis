9c32be8031a3997494aab57ddacf4a7cc4a1cd8c
Edgar Dueñas <edmadu12@gmail.com>
2020-03-11 20:26:24 -0600

    .

--- activos_fijos/activos_fijos.php
+++ activos_fijos/activos_fijos.php
@@ -1,108 +1,256 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<title>Stock</title>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <!-- ESTILOS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Activos fijos</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="../css/activos_fijos.css"">
-    <link rel="shortcut icon" href="img/favicon.ico" />
-    <!-- Fuentes de iconos -->
+        <link rel="stylesheet" type="text/css" href="../css/activos_fijos.css"">
+    <link rel="shortcut icon" href="../img/favicon.ico" />
+    <!-- Tipografias -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- SCRIPT -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat+Subrayada&display=swap" rel="stylesheet">
+    <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script>//Script para abrir ventana de tamaño asignado.
+        function generar_QR(url) {
+            open(url,'','top=300,left=300,width=300,height=300') ;
+        }
+    </script>
 </head>
+
 <body>
-	<h1>Inventario</h1>
-	<div class="contenedor">
-		<h2>CPU</h2>
-			<?php
-				require '../estaciones/conexion.php';
-				$conectar 		= 	mysqli_connect($servidor, $usuario, $pass, $basededatos);
-           		$select = "SELECT * FROM cpu";
-           		$resul = mysqli_query($conectar, $select);
-           		$contador_cpu=1;
-           	?>
-       		<table class="table table-striped table-dark table-hover" id="cpu-id">
-	           	<thead class="cabecera_tabla">
-		           	<tr>
-		           		<th>Marca</th>
-		           		<th>Número de serie</th>
-		           		<th>Modelo </th>
-		           		<th>Dirección MAC</th>
-		           		<th>Ubicación</th>
-		                <th>Comentarios</th>
-		            </tr>
-	            </thead>
-	            <tbody class="cuerpo_tabla">
-	            	<form action="estaciones/estacion.php" method="post" target="_blank">
-	                <?php
-	            		while( $row = mysqli_fetch_assoc($resul)) {
-	                		echo "<tr>";
-	                		echo "
-	            					<td id='user_".$contador_cpu."'>".$row['marca_cpu']."</td>
-	            					<td>".$row['serie_cpu']."</td>
-	            					<td>".$row['modelo_cpu']."</td>
-	            					<td>".$row['mac_address']."</td>
-	            					<td>".$row['ubicacion']."</td>
-	            					<td>".$row['comentario']."</td>";
-	            	?>
-	            					<td>
-	            						
-                  							<input name='codigoQR_cpu' id='' class='button_xdx btn btn-dark btn-lg' type='submit' value='Generar QR'/>
-						                
-	            					</td>
-	            	<?php
-	                		echo "</tr>";
-	            		}
-	            	?>
-	            	</form>
-				</tbody>
-			</table>
-	</div>
+    <?php
+        require '../estaciones/conexion.php';
+        $conectar       =   mysqli_connect($servidor, $usuario, $pass, $basededatos);
+        //querys
+        $consulta_cpu_operacion     =   "SELECT COUNT(*) ubicacion FROM cpu WHERE ubicacion != 'Almacen'";
+        $consulta_cpu_almacen       =   "SELECT COUNT(*) ubicacion FROM cpu WHERE ubicacion = 'Almacen'";
 
-	<div class="contenedor">
-		<h2>Monitor</h2>
-			<?php
-				$select = "SELECT * FROM monitor";
+        $consulta_monitor_operacion =   "SELECT COUNT(*) ubicacion FROM monitor WHERE ubicacion !='Almacen' ";
+        $consulta_monitor_almacen   =   "SELECT COUNT(*) ubicacion FROM monitor WHERE ubicacion ='Almacen' ";
 
-            		$resul = mysqli_query($conectar, $select);
+        $consulta_diadema_operacion =   "SELECT COUNT(*) ubicacion FROM diademas WHERE ubicacion != 'Almacen'";
+        $consulta_diadema_almacen   =   "SELECT COUNT(*) ubicacion FROM diademas WHERE ubicacion = 'Almacen'";
 
-            		$contador_monitor=1;
-            ?>
-            <table class="table table-striped table-dark" id="monitor-id">
-	           	<thead class="cabecera_tabla">
-		           	<tr>
-		           		<th>Marca</th>
-		           		<th>Número de serie</th>
-		           		<th>Modelo </th>
-		           		<th>Ubicación</th>
-		                <th>Comentarios</th>
-		            </tr>
-	            </thead>
-	            <tbody class="cuerpo_tabla">
-	                <?php
-	            		while( $row = mysqli_fetch_assoc($resul)) {
-	                		echo "<tr>";
-	                		echo "
-	            					<td id='user_".$contador_monitor."'>".$row['marca_monitor']."</td>
-	            					<td>".$row['serie_monitor']."</td>
-	            					<td>".$row['modelo_monitor']."</td>
-	            					<td>".$row['ubicacion']."</td>
-	            					<td>".$row['comentario']."</td>";
-	                		echo "</tr>";
-	            		}
-	            	?>
-				</tbody>
-			</table>
-			
-		
-	</div>
-</body> 
-</html>
+        $consulta_mouse_operaciones =   "SELECT COUNT(*) ubicacion FROM mouse WHERE ubicacion != 'Almacen'";
+        $consulta_mouse_almacen     =   "SELECT COUNT(*) ubicacion FROM mouse WHERE ubicacion = 'Almacen'";
+
+        $consulta_teclado_operaciones =   "SELECT COUNT(*) ubicacion FROM teclados WHERE ubicacion != 'Almacen'";
+        $consulta_teclado_almacen     =   "SELECT COUNT(*) ubicacion FROM teclados WHERE ubicacion = 'Almacen'";
+
+        
+        //conexion y ejecucion de querys
+        $resul              =   mysqli_query($conectar, $consulta_cpu_operacion);
+        $resul2             =   mysqli_query($conectar, $consulta_cpu_almacen);
+
+        $resul_monitor      =   mysqli_query($conectar, $consulta_monitor_operacion);
+        $resul_monitor2     =   mysqli_query($conectar, $consulta_monitor_almacen);
+
+        $resul_diadema      =   mysqli_query($conectar, $consulta_diadema_operacion);
+        $resul_diadema2     =   mysqli_query($conectar, $consulta_diadema_almacen);
+
+        $resul_mouse        =   mysqli_query($conectar, $consulta_mouse_operaciones);
+        $resul_mouse2       =   mysqli_query($conectar, $consulta_mouse_almacen);
+
+        $resul_teclado        =   mysqli_query($conectar, $consulta_teclado_operaciones);
+        $resul_teclado2       =   mysqli_query($conectar, $consulta_teclado_almacen);
+
+        
+        //resultados
+        $fila   =   mysqli_fetch_assoc($resul);
+        $fila2  =   mysqli_fetch_assoc($resul2);
+
+        $fila3  =   mysqli_fetch_assoc($resul_monitor);
+        $fila4  =   mysqli_fetch_assoc($resul_monitor2);
+
+        $fila5  =   mysqli_fetch_assoc($resul_diadema);
+        $fila6  =   mysqli_fetch_assoc($resul_diadema2);
+
+        $fila7  =   mysqli_fetch_assoc($resul_mouse);
+        $fila8  =   mysqli_fetch_assoc($resul_mouse2);
+
+        $fila9  =   mysqli_fetch_assoc($resul_teclado);
+        $fila10  =   mysqli_fetch_assoc($resul_teclado2);
+    ?>
+    <h1>Activos fijos</h1>
+    <br>
+    <div class="container-fluid">
+        <section class="row datos_generales" id="stock">
+            <table class="table table-dark">
+                <thead class="cabecera_tabla">
+                    <tr>
+                        <th>PISO</th>
+                        <th>ALMACÉN</th>
+                        <th>PISO</th>
+                        <th>ALMACÉN</th>
+                        <th>PISO</th>
+                        <th>ALMACÉN</th>
+                        <th>PISO</th>
+                        <th>ALMACÉN</th>
+                        <th>PISO</th>
+                        <th>ALMACÉN</th>
+                    </tr>
+                </thead>
+                <tbody class="cuerpo_tabla">
+                    <?php
+                        
+                        echo "<tr>";
+                            echo "<td colspan='2' class='texto'><a href='#cpu-id'>CPU</a></td>";
+                            echo '<td colspan="2" class="texto"><a href="#monitor-id">Monitor</a></td>';
+                            echo '<td colspan="2" class="texto"><a href="#diadema-id">Diadema</a></td>';
+                            echo '<td colspan="2" class="texto">Mouse</td>';
+                            echo '<td colspan="2" class="texto">Teclado</td>';
+                        echo "</tr>";
+                        echo "<tr>";
+                            //Sección conteo CPU
+                            echo "<td class='texto'><i class='material-icons'>smartphone</i><br>".$fila['ubicacion']."</td>";
+                            echo "<td class='texto'><i class='material-icons'>smartphone</i><br>".$fila2['ubicacion']."</td>";
+                            //Sección conteo Monitor
+                            echo "<td class='texto'><i class='material-icons'>desktop_windows</i><br>".$fila3['ubicacion']."</td>";
+                            echo "<td class='texto'><i class='material-icons'>desktop_windows</i><br>".$fila4['ubicacion']."</td>";
+                            //Sección conteo diadema
+                            echo "<td class='texto'><i class='material-icons'>headset_mic</i><br>".$fila5['ubicacion']."</td>";
+                            echo "<td class='texto'><i class='material-icons'>headset_mic</i><br>".$fila6['ubicacion']."</td>";
+                            //Sección conteo mouse
+                            echo "<td class='texto'><i class='material-icons'>mouse</i><br>".$fila7['ubicacion']."</td>";
+                            echo "<td class='texto'><i class='material-icons'>mouse</i><br>".$fila8['ubicacion']."</td>";
+                            //Sección conteo teclado
+                            echo "<td class='texto'><i class='material-icons'>keyboard</i><br>".$fila9['ubicacion']."</td>";
+                            echo "<td class='texto'><i class='material-icons'>keyboard</i><br>".$fila10['ubicacion']."</td>";
+                        echo "</tr>";
+                    ?>
+                </tbody>
+            </table>
+        </section>
+        <h2 id="cpu-id">CPU</h2>
+        <section class="row datos_generales">
+            <table class="table table-dark table-hover">
+                <thead class="cabecera_tabla">
+                    <tr>
+                        <th>MARCA</th>
+                        <th>Serie</th>
+                        <th>Modelo</th>
+                        <th>Direccion MAC</th>
+                        <th>Ubicación</th>
+                        <th>Área</th>
+                        <th>Comentario</th>
+                        <th>Codigo QR</th>
+                    </tr>
+                </thead>
+                <tbody class="cuerpo_tabla">
+                    <?php
+                        $consulta_general_cpu       =   "SELECT * FROM cpu";
+                        $res_consulta_general_cpu   =   mysqli_query($conectar, $consulta_general_cpu);
+                        $contador=1;
+                        
+                        while( $fila_general   =   mysqli_fetch_assoc($res_consulta_general_cpu)) {
+                echo "<tr>";
+                echo "
+                    <td class='user_".$contador."'>".$fila_general['marca_cpu']."</td>
+                    <td>".$fila_general['serie_cpu']."</td>
+                    <td>".$fila_general['modelo_cpu']."</td>
+                    <td>".$fila_general['mac_address']."</td>
+                    <td>".$fila_general['ubicacion']."</td>
+                    <td>".$fila_general['area']."</td>
+                    <td>".$fila_general['comentario']."</td>";
+                    ?>
+                    <td>
+                        <form action="activos_fijos/cpu.php" method="post" target="_blank">
+                            <input name='codigo_qr' id='<?=$fila_general['uniqueid']?>' class='button_xdx btn btn-dark btn-md' type='submit' value='QR <?=$fila_general['uniqueid']?>'/>
+                        </form>
+                    </td>
+                    <?php                  
+                echo"</tr>";
+            }
+                    ?>
+                </tbody>
+            </table>
+        </section>
+        <br>
+        <h2 id="monitor-id">Monitores</h2>
+        <section class="row datos_generales">
+            <table class="table table-dark table-hover">
+                <thead class="cabecera_tabla">
+                    <tr>
+                        <th>MARCA</th>
+                        <th>Serie</th>
+                        <th>Modelo</th>
+                        <th>Ubicación</th>
+                        <th>Área</th>
+                        <th>Comentario</th>
+                        <th>Codigo QR</th>
+                    </tr>
+                </thead>
+                <tbody class="cuerpo_tabla">
+                    <?php
+                        $consulta_general_monitor       =   "SELECT * FROM monitor";
+                        $res_consulta_general_monitor   =   mysqli_query($conectar, $consulta_general_monitor);
+                        $contador=1;
+                        
+                        while( $fila_general   =   mysqli_fetch_assoc($res_consulta_general_monitor)) {
+                echo "<tr>";
+                echo "
+                    <td class='user_".$contador."'>".$fila_general['marca_monitor']."</td>
+                    <td>".$fila_general['serie_monitor']."</td>
+                    <td>".$fila_general['modelo_monitor']."</td>
+                    <td>".$fila_general['ubicacion']."</td>
+                    <td>".$fila_general['area']."</td>
+                    <td>".$fila_general['comentario']."</td>";
+                    ?>
+                    <td>
+                        <form action="activos_fijos/monitor.php" method="post" target="_blank">
+                            <input name='codigo_qr' id='<?=$fila_general['uniqueid']?>' class='button_xdx btn btn-dark btn-md' type='submit' value='QR <?=$fila_general['uniqueid']?>'/>
+                        </form>
+                    </td>
+                    <?php                  
+                echo"</tr>";
+            }
+                    ?>
+                </tbody>
+            </table>
+        </section>
+        <br>
+        <h2 id="diadema-id">Diademas</h2>
+        <section class="row datos_generales">
+            <table class="table table-dark table-hover">
+                <thead class="cabecera_tabla">
+                    <tr>
+                        <th>MARCA</th>
+                        <th>Serie</th>
+                        <th>Ubicación</th>
+                        <th>Área</th>
+                    </tr>
+                </thead>
+                <tbody class="cuerpo_tabla">
+                    <?php
+                        $consulta_general_diadema       =   "SELECT * FROM diademas";
+                        $res_consulta_general_diadema   =   mysqli_query($conectar, $consulta_general_diadema);
+                        $contador=1;
+                        
+                        while( $fila_general   =   mysqli_fetch_assoc($res_consulta_general_diadema)) {
+                echo "<tr>";
+                echo "
+                    <td class='user_".$contador."'>".$fila_general['marca_diadema']."</td>
+                    <td>".$fila_general['num_serie']."</td>
+                    <td>".$fila_general['ubicacion']."</td>
+                    <td>".$fila_general['area']."</td>";
+                                      
+                echo"</tr>";
+            }
+                    ?>
+                </tbody>
+            </table>
+        </section>
+    </div>
+
+<!--li>
+<a href="javascript:generar_QR('activos_fijos/generarQR.php')">Eliminar</a>
+</li-->
+
+</div>
+</body>
+</html> 
--- activos_fijos/cpu.php
+++ activos_fijos/cpu.php
@@ -6,63 +6,72 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
-    <title>CPU QR</title>
+    <meta charset="UTF-8">
+    <title>Document</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
             <div class="col-3" id="datos">
-                <?php 
-                    $consulta_cpu   =   "SELECT * FROM cpu WHERE uniqueid = '$cpu'";
-                    $res_de_consulta=   mysqli_query($conectar, $consulta_cpu);
-                    while( $fila_general   =   mysqli_fetch_assoc($res_consulta_general_cpu)) {
-                echo "<tr>";
-                    echo "<td>ID</td>";
-                    echo "<td>".$fila_general['uniqueid']."</td>";
-                echo "</tr>";
+                <table>
+                    <?php 
+                        echo "$cpu";
+                        $consulta_cpu   =   "SELECT * FROM cpu WHERE uniqueid = '$cpu'";
+                        $res_de_consulta=   mysqli_query($conectar, $consulta_cpu);
+                        while( $fila_general   =   mysqli_fetch_assoc($res_de_consulta)) {
+                    echo "<thead>";
+                        echo "<tr>";
+                            echo "<td>ID</td>";
+                            $id=$fila_general['uniqueid'];
+                            echo "$id";
+                            echo "<td>".$fila_general['uniqueid']."</td>";
+                        echo "</tr>";
+                    echo "</thead>";
+                    echo "<tbody>";
+                        echo "<tr>";
+                            echo "<td>Marca</td>";
+                            echo "<td>".$fila_general['marca_cpu']."</td>";
+                        echo "</tr>";
 
-                echo "<tr>";
-                    echo "<td>Marca</td>";
-                    echo "<td>".$fila_general['marca_cpu']."</td>";
-                echo "</tr>";
+                        echo "<tr>";
+                            echo "<td>Serie</td>";
+                            echo "<td>".$fila_general['serie_cpu']."</td>";
+                        echo "</tr>";
 
-                echo "<tr>";
-                    echo "<td>Serie</td>";
-                    echo "<td>".$fila_general['serie_cpu']."</td>";
-                echo "</tr>";
+                        echo "<tr>";
+                            echo "<td>Modelo</td>";
+                            echo "<td>".$fila_general['modelo_cpu']."</td>";
+                        echo "</tr>";
 
-                echo "<tr>";
-                    echo "<td>Modelo</td>";
-                    echo "<td>".$fila_general['modelo_cpu']."</td>";
-                echo "</tr>";
+                        echo "<tr>";
+                            echo "<td>Direccion MAC</td>";
+                            echo "<td>".$fila_general['mac_address']."</td>";
+                        echo "</tr>";
 
-                echo "<tr>";
-                    echo "<td>Direccion MAC</td>";
-                    echo "<td>".$fila_general['mac_address']."</td>";
-                echo "</tr>";
+                        echo "<tr>";
+                            echo "<td>Ubicacion</td>";
+                            echo "<td>".$fila_general['ubicacion']."</td>";
+                        echo "</tr>";
 
-                echo "<tr>";
-                    echo "<td>Ubicacion</td>";
-                    echo "<td>".$fila_general['ubicacion']."</td>";
-                echo "</tr>";
+                        echo "<tr>";
+                            echo "<td>Área</td>";
+                            echo "<td>".$fila_general['area']."</td>";
+                        echo "</tr>";
 
-                echo "<tr>";
-                    echo "<td>Área</td>";
-                    echo "<td>".$fila_general['area']."</td>";
-                echo "</tr>";
-
-                echo "<tr>";
-                    echo "<td>Comentario</td>";
-                    echo "<td>".$fila_general['comentario']."</td>";
-                echo "</tr>";
-                ?>
+                        echo "<tr>";
+                            echo "<td>Comentario</td>";
+                            echo "<td>".$fila_general['comentario']."</td>";
+                        echo "</tr>";
+                    echo "</tbody>";
+                }
+                    ?>
+                </table>
             </div>
         </div>          
     </div>
 </body>
-</html>
+</html> 
