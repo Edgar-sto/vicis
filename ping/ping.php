0be2fd2961d6f0d3b90e818147a1985637729d05
Edgar Dueñas <edmadu12@gmail.com>
2020-04-13 18:24:30 -0500

    .

--- ping/ping.php
+++ ping/ping.php
@@ -1,13 +1,23 @@
 <?php 
-	$ping	=	$_POST['ping'];
 
-	$res	= shell_exec("ping $ping");
+	$ping	=	$_POST['valorCaja1'];
+
+	$res	= exec("ping $ping");
 	
 	if (strpos($res, "recibidos = 0")) {
 		echo "Nose pudo hacer ping";
 	} else {
 		echo "Ping exitoso";
+		echo "$res";
 	}
+
+	echo "<br>";
+
+	$output = shell_exec('ls'); 
+  
+// Display the list of all file 
+// and directory 
+echo "<pre>$output</pre>"; 
 	/*exec("$ping", $output);
 	foreach ($output as $x) {
 		print_r($x."<br>");
