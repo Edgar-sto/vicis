b45af7e4e4d85dbc73be9fad4ad6963a4e2b67d2
Edgar Dueñas <edmadu12@gmail.com>
2020-05-15 00:18:44 -0500

    Update ping.php

--- ping/ping.php
+++ ping/ping.php
@@ -2,12 +2,16 @@
 
 	$ping	=	$_POST['valorCaja1'];
 
-	$res	= exec("ping -c 10 $ping");
+	$res	= shell_exec("ping $ping");
 	
+	echo  "<pre>".$res."</pre>";
+
+
+	/*
 	if (strpos($res, "recibidos = 0")) {
 		echo "Nose pudo hacer ping";
 	} else {
 		echo "Ping exitoso <br>";
 		echo "$res";
-	}
+	}*/
  ?>
