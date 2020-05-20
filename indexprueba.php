86c6212c110a953c410e7f2676133932dc77d617
Edgar Dueñas <edmadu12@gmail.com>
2020-05-19 20:01:18 -0500

    .

---
+++ indexprueba.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Panel ViciDial</title>
+        <!-- Stylesheets -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/estilos.css">
+        <!-- Javascript -->
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script> 
+        <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+</head>
+<body>
+	<div class="container_fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col"></div>
+				<table class="table table-bordered table-dark">
+				 	<thead>
+				    	<tr>
+				      		<th scope="col">#</th>
+				      		<th scope="col">Servidor</th>
+						    <th scope="col">Id campaña</th>
+						    <th scope="col">Handle</th>
+				    	</tr>
+				 	</thead>
+				 	<tbody>
+				    	<tr>
+				     		<th scope="row">1</th>
+				     		<td>Mark</td>
+				      		<td>Otto</td>
+				      		<td>@mdo</td>
+				    	</tr>
+					    <tr>
+						    <th scope="row">2</th>
+						    <td>Jacob</td>
+						    <td>Thornton</td>
+						    <td>@fat</td>
+					    </tr>
+					    <tr>
+						    <th scope="row">3</th>
+						    <td colspan="2">Larry the Bird</td>
+						    <td>@twitter</td>
+					    </tr>
+				 	</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
