ec82f9cf2d389bba23c2cdf1486ef62b707bb261
Edgar Dueñas <edmadu12@gmail.com>
2020-05-20 23:06:11 -0500

    .

--- indexprueba.php
+++ indexprueba.php
@@ -93,7 +93,67 @@
                 });
             });
         </script>
-    </head>
+        <script type="text/javascript" language="javascript">
+			function refreshDivs(divid,secs,url)
+			{
+
+			// define our vars
+			var divid,secs,url,fetch_unix_timestamp;
+
+			// Chequeamos que las variables no esten vacias..
+			if(divid == ""){ alert('Error: escribe el id del div que quieres refrescar'); return;}
+			else if(!document.getElementById(divid)){ alert('Error: el Div ID selectionado no esta definido: '+divid); return;}
+			else if(secs == ""){ alert('Error: indica la cantidad de segundos que quieres que el div se refresque'); return;}
+			else if(url == ""){ alert('Error: la URL del documento que quieres cargar en el div no puede estar vacia.'); return;}
+
+			// The XMLHttpRequest object
+
+			var xmlHttp;
+			try{
+			xmlHttp=new XMLHttpRequest(); // Firefox, Opera 8.0+, Safari
+			}
+			catch (e){
+			try{
+			xmlHttp=new ActiveXObject("Msxml2.XMLHTTP"); // Internet Explorer
+			}
+			catch (e){
+			try{
+			xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch (e){
+			alert("Tu explorador no soporta AJAX.");
+			return false;
+			}
+			}
+			}
+
+			// Timestamp para evitar que se cachee el array GET
+
+			fetch_unix_timestamp = function()
+			{
+			return parseInt(new Date().getTime().toString().substring(0, 10))
+			}
+
+			var timestamp = fetch_unix_timestamp();
+			var nocacheurl = url+"?t="+timestamp;
+
+			// the ajax call
+			xmlHttp.onreadystatechange=function(){
+			if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+			document.getElementById(divid).innerHTML=xmlHttp.responseText;
+			setTimeout(function(){refreshDivs(divid,secs,url);},secs*1000);
+			}
+			}
+			xmlHttp.open("GET",nocacheurl,true);
+			xmlHttp.send(null);
+			}
+
+			// LLamamos las funciones con los repectivos parametros de los DIVs que queremos refrescar.
+			window.onload = function startrefresh(){
+			refreshDivs('campañas',30000,'info_campanias.php');
+			}
+		</script>
+</head>
 <body>
 	<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
             <!-- Brand -->
@@ -180,7 +240,7 @@
     <div class="rowb fila_uno">
     	<div class="col-md-1"></div>
   		<div class="col-md-4">
-  			<h2 class="subtitulos">Informacion por servidor</h2>
+  			<h2 class="subtitulos">Servidores</h2>
   			<br>
   			<table class="table">
   				<caption class="cabecera_tbl">(Acceso a VICIdial)</caption>
@@ -265,7 +325,7 @@
   		<div class="col-md-2"></div>
   		<div class="col-md-4">
   			<h2 class="subtitulos">Informacion por servidor</h2>
-  			<div class='tm-notification-items'>
+  			<div id="campañas" class='tm-notification-items'>
   			<?php
   				require 'info_campanias/info_campanias.php';
   			?>
