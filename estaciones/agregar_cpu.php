3f5e37f7271b3942384d3498542d560b942b782a
Edgar Dueñas <edmadu12@gmail.com>
2021-03-30 14:29:26 -0600

    .

--- estaciones/agregar_cpu.php
+++ estaciones/agregar_cpu.php
@@ -10,35 +10,35 @@
 	$area			=	$_POST['area'];
 	$comentary  	=	$_POST['comentario'];
 	
-	/*
+	
 	echo "$marcaCpu";
 	echo "$numSerieCpu";
 	echo "$modeloCpu";
 	echo "$mac";
 	echo "$ubicacion";
 	echo "$area";
-	echo "$comentary";*/
+	echo "$comentary";
 	echo "<br>";
 
-	$conectar 		= 	mysqli_connect($servidor, $usuario, $pass, $basededatos);
+	//$conectar 		= 	mysqli_connect($servidor, $usuario, $pass, $basededatos);
 
-	$insertar = "INSERT INTO cpu_oficial (marca_cpu, serie_cpu, modelo_cpu, mac_address, ubicacion, area, comentario) VALUES ( '$marcaCpu', '$numSerieCpu', '$modeloCpu', '$mac', '$ubicacion', '$area', '$comentary')";
-	$query = mysqli_query($conectar, $insertar);
+	//$insertar = "INSERT INTO cpu_oficial (marca_cpu, serie_cpu, modelo_cpu, mac_address, ubicacion, area, comentario) VALUES ( '$marcaCpu', '$numSerieCpu', '$modeloCpu', '$mac', '$ubicacion', '$area', '$comentary')";
+	//$query = mysqli_query($conectar, $insertar);
 
 	//Codigo QR
 	$nombre_de_imagen = $ubicacion;
-    $content = "$marcaCpu, $numSerieCpu, $modeloCpu, $mac, $ubicacion, $area, $comentary";
+    $content = "Edgar, dueñas";
     //QRcode::png ($contenido, $archivo, $ecc, $tamaño, $margen)
     QRcode::png(
     $content,                                           //CONTENIDO
-    "../img/codigos_qr/cpu/".$nombre_de_imagen.".png",  //NOMBRE DEL ARCHIVO
+    "../img/".$nombre_de_imagen.".png",  //NOMBRE DEL ARCHIVO
     QR_ECLEVEL_L,                                       //INDICE DE CORRECION DE ERROREES
     6,                                                  //TAMAÑO EN PIXELES
     1,                                                  //TAMAÑO DEL MARGEN
     );
 
 
-	if ($query) {
+	if (QRcode) {
 		echo "<script>
 				alert('Equipo guardado');
 				window.close();
